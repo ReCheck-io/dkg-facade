39eb31887d5300c91f2fed17e4388ea1b7a6b80a
Ivaylo Rusev <irusev@recheck.io>
2023-03-27 19:11:51 +0300

    express app server . listen all interfaces instead of localhost only

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ app.use(bodyParser.urlencoded({
 console.log(config);
 
 
-var server = app.listen(config.server_port,  "localhost", function () {
+var server = app.listen(config.server_port,  function () {
   console.log("DKGFacade listening at http://localhost:%s", server.address().port)
 });
 
